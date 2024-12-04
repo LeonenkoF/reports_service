5e4d2c75e89d498bf56d52e3e4b163fff3a8d9da
Michail Borisenko <bmaa@yandex.ru>
2024-12-04 16:55:27 +0300

    041224 RS-2 db design bma. sql files 2

--- storage/dbcr.go
+++ storage/dbcr.go
@@ -7,33 +7,7 @@ var (
 	CREATE TYPE STAGE_TYPE AS ENUM ('new','inprogress','done', 'canceled');
 	CREATE TYPE ROLE_TYPE AS ENUM ('admin','user','superadmin','superuser','guest');
 `
-	/*
-	   	schTypes = `
-	   	CREATE TYPE IF NOT EXISTS PRIORITY_TYPE AS ENUM ('high','medium','low');
-	   	CREATE TYPE IF NOT EXISTS STAGE_TYPE AS ENUM ('new','inprogress','done', 'canceled');
-	   	CREATE TYPE IF NOT EXISTS ROLE_TYPE AS ENUM ('admin','user','superadmin','superuser','guest');
-	   `
-	*/
-	// report - таблица жалоб
-	/*
-		id				порядковый номер жалобы
-		uuid 			идентификация жалобы (по ТЗ)
-		user_uuid 		индентификатор автора-пользователя
-		description 	текст жалобы
-		priority		приоритет [Высокий, Средний, Низкий]
-		stage			стадия обработки.
-						*** Может быть enum но более гибко завести справочник под редакцией админа. Возможно пополнение стадий.
-		topic			тема жалобы.
-						*** Рекомендации те же чтои у stage
-		group 			группа жалобы. может отсутсвовать
-						*** Варианты:
-						1. Каждой группе своя таблица с номерами жалоб, как и в случае хранения диалога по жалобе (comments)
-						2. Таблица одна с парами (группа, имя группы, жалоба). Тогда будет дублирование данных по группам. Возможно легче обработка (поиск).
-
-		adm_uuid		идентификатор админа, который закрывал жалобу и писал closing_comment
-		closing_comment	коментарий админа при закрытии жалобы
-						*** Возможно поле избыточно, но было бы интересно чем в итоге закончилась жалоба
-	*/
+	//
 	schReport string = `
 CREATE TABLE IF NOT EXISTS reports (
 	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -69,7 +43,6 @@ CREATE TABLE IF NOT EXISTS groupps (
 );
 CREATE INDEX IF NOT EXISTS idx_id ON groupps (group_name);
 `
-	//CREATE INDEX IF NOT EXISTS idx_id ON groupps (id);
 
 	// таблица диалога по жалобе. каждому диалогу своя таблица
 	/*
@@ -89,17 +62,6 @@ CREATE TABLE IF NOT EXISTS comments (
     updated_at DATE NOT NULL DEFAULT CURRENT_DATE	
 );
 `
-	// таблица пользователей
-	/*
-	   	id 			идентификатор
-	   	user_uuid 	UUID,
-	   	mail 		почта
-	   	phone 		номер телефона (*** не обязательно)
-	   	nick 		ник (*** необязательно)
-	   	role INT	роль (user admin *** superuser *** superadmin)
-	       created_at 	дата регистрации
-	       updated_at 	дата инзменения (*** не обязательно)
-	*/
 	schUsers string = `
 CREATE TABLE IF NOT EXISTS users (
 	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -112,15 +74,6 @@ CREATE TABLE IF NOT EXISTS users (
     updated_at DATE NOT NULL DEFAULT CURRENT_DATE	
 );
 `
-	// список стадий обработки *** возможно нужно просто enum, но возможное появление новых стадий будет вызывать переписывание кода
-	/*
-	   	schStage string = `
-	   CREATE TABLE IF NOT EXISTS stages (
-	   	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	   	stage VARCHAR(10)
-	   );
-	   `
-	*/
 	// список тем *** возможно нужно просто enum, но возможное появление новых тем будет вызывать переписывание кода
 	schCategory string = `
 CREATE TABLE IF NOT EXISTS categoryes (
@@ -128,22 +81,4 @@ CREATE TABLE IF NOT EXISTS categoryes (
 	category VARCHAR(32)
 );	
 `
-
-	// список ролей участников процесса *** возможно нужно просто enum, но возможное появление новых ролей будет вызывать переписывание кода
-/*
-	schRole string = `
-CREATE TABLE IF NOT EXISTS roles (
-	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	role VARCHAR(10)
-)
-`
-*/
-/*
-	schPriority string = `
-CREATE TABLE IF NOT EXISTS roles (
-	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	priority VARCHAR(10)
-)
-`
-*/
 )
