6ea3b4bde08a655a2a6888b0aef572ff280242e2
Borisenko Mikhail <bmaa@yandex.ru>
2024-12-06 10:25:24 +0300

    [RS-2] Designing a database for coplaints. bma 11

--- migration/groupps.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS groupps (
-	groupp UUID,
-	group_name VARCHAR(32),
-	rep_id INT
-);
-CREATE INDEX IF NOT EXISTS idx_id ON groupps (group_name);
--- migration/types.sql
+++
@@ -1,8 +0,0 @@
-/*
-	Типы перечислений проекта
-	*** ROLE_TYPE намеренно расширен дополнительными ролями. Вопрос на обсуждении.
-*/
-	CREATE TYPE PRIORITY_TYPE AS ENUM ('high','medium','low');
-	CREATE TYPE STAGE_TYPE AS ENUM ('new','inprogress','done', 'canceled');
-	CREATE TYPE ROLE_TYPE AS ENUM ('admin','user','superadmin','superuser','guest');
-    
